64b4dd5ac17605f4eea80d4327b2bfce22ae39b3
Jolene Langlinais <jolenelanglinais@gmail.com>
2019-06-23 13:00:36 -0400

    develop sidebar content

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
 ---- MODEL ----
@@ -36,10 +36,32 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/headshot.jpg" ] []
-        , h1 [] [ text "Elm!" ]
+    div [ class "sideBar" ]   
+        [ a [ href "https://jolenelanglinais.com" ] [ img [ src "/headshot.jpg" ] [] ] 
+        , a [ href "https://jolenelanglinais.com" ] [ h1 [] [ text "Jolene Langlinais" ] ] 
+        , p []
+        [ text "A full stack software engineer at "
+        , a [ href "https://clause.io" , target "_blank" , class "headerLink" ] [ text "Clause.io"] 
+        , text " and maintainer at the "
+        , a [ href "https://www.accordproject.org" , target "_blank" , class "headerLink"  ] [ text "Accord Project"] 
+        , text ", creating sophisticated systems with code and design."
         ]
+        , div [ class "socialLink" ] [
+            a [ href "https://www.linkedin.com/in/jolene-langlinais/" , target "_blank" ] [ i [ class "fa fa-linkedin" ] []  ]
+           , a [ href "https://twitter.com/jolanglinais" , target "_blank" ] [ i [ class "fa fa-twitter" ] []  ]
+           , a [ href "https://medium.com/@jolene.langlinais" , target "_blank" ] [ i [ class "fa fa-medium" ] []  ]
+           , a [ href "https://github.com/irmerk" , target "_blank" ] [ i [ class "fa fa-github" ] [] ]
+        ]
+        , div [ class "navLink" ] [ 
+            a [ href "resume" ] [ text "Resume" ] 
+        ]
+        , div [ class "navLink" ] [ 
+            a [ href "projects" ] [ text "Projects" ] 
+        ]
+        , div [ class "navLink" ] [ 
+            a [ href "blog" ] [ text "Blog" ] 
+        ]
+    ]
 
 
 
